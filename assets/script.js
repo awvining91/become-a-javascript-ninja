8e6598a3f4cd6f08afa3c3aa4b45fa6461f668cb
Will Vining <awvining91@gmail.com>
2022-08-21 16:22:52 -0700

    massive rework i think the start button will start the quiz now

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+
+
+document.querySelector('#generate').addEventListener('click', promptMe2);
+
+function promptMe2() {
+
+    var questions = [
+        {
+            prompt: "What color are apples?\n(a) Red/Green\n\(b) Purple\n(c) Orange",
+            answer: "a"
+        },
+        {
+            prompt: "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow",
+            answer: "c"
+        },
+        {
+            prompt: "What color are strawberries?\n(a) Yellow\n\(b) Red\n(c) Blue",
+            answer: "a"
+        }
+    
+    
+    ];
+    
+        var score = 0;
+    
+        //used this video as reference for questionnaire section https://youtu.be/LQGTb112N_c
+    
+        for(var i=0; i < questions.length; i++){
+        var response = window.prompt(questions[i].prompt);
+        if(response == questions[i].answer){
+            score++;
+            alert("Correct!");
+        } else {
+            alert("WRONG! Time deducted!");
+        }
+    
+    }
+    
+        alert("You got "+ score + "/" + questions.length + "question(s) right!");
+    //used this as reference for for loop for questionnaie https://www.youtube.com/watch?v=LQGTb112N_c
+    
+
+
+}
