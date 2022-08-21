b02882451deca81169327b0f444d36f368ba858e
Will Vining <awvining91@gmail.com>
2022-08-21 14:15:17 -0700

    started over again

---
+++ script.js
@@ -0,0 +1,14 @@
+var questions = [
+    {
+        prompt: "What color are apples?\n(a) Red/Green\n\
+        (b) Purple/n(c) Orange",
+        answer: "a"
+    },
+
+
+
+
+]
+var score = 0;
+
+//used this video as reference for questionnaire section https://youtu.be/LQGTb112N_c
